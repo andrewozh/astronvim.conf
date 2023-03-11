a6e2c8fa79135758d1b8708744bf2c60756ed9a3
andrewozh <andrew.ozhegov@gmail.com>
2023-03-11 17:53:21 +0300

    unsure all required language highlighters are installed

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,21 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+        "bash",
+        "regex",
+        "awk",
+        "python",
+        "javascript",
+        "go",
+        "yaml",
+        "json",
+        "terraform",
+        "dockerfile",
+        "hcl",
+        "lua",
+        "vim",
+        "markdown",
+        "help"
     })
   end,
 }
