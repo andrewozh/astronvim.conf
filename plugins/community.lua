396747b029097ca49a608dcf6c9fd1f2b43f3505
Andrew Ozhegov <andrew.ozhegov@gmail.com>
2023-09-30 18:28:21 +0300

    community copilot

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,21 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.copilot-lua" },
+  -- {
+  --   "copilot.lua",
+  --   enable = false,
+  --   opts = {
+  --     suggestion = {
+  --       keymap = {
+  --         accept = "<C-l>",
+  --         accept_word = false,
+  --         accept_line = false,
+  --         next = "<C-.>",
+  --         prev = "<C-,>",
+  --         dismiss = "<C/>",
+  --       },
+  --     },
+  --   },
+  -- },
 }
