1c9452756a57c472f2fb6fa70afc93abe9cbd582
andrewozh <andrew.ozhegov@gmail.com>
2024-06-08 20:02:58 +0300

    setup gruvbox colorsheme

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,38 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
 
 ---@type LazySpec
+-- return {
+--   "AstroNvim/astrocommunity",
+--   { import = "astrocommunity.pack.lua" },
+--   -- import/override with your plugins folder
+-- }
+
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  {
+    "ellisonleao/gruvbox.nvim",
+    as = "gruvbox",
+    config = function()
+      require("gruvbox").setup({
+          undercurl = true,
+          underline = true,
+          bold = true,
+          italic = {},
+          strikethrough = true,
+          invert_selection = false,
+          invert_signs = false,
+          invert_tabline = true,
+          invert_intend_guides = false,
+          inverse = false, -- invert background for search, diffs, statuslines and errors
+          contrast = "soft", -- can be "hard", "soft" or empty string
+          palette_overrides = {},
+          overrides = {},
+          dim_inactive = false,
+          transparent_mode = false,
+      })
+    end,
+  },
 }
--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
@@ -11,7 +11,7 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "astrodark",
+    colorscheme = "gruvbox",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
@@ -20,6 +20,9 @@ return {
       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },
       },
+      gruvbox = {
+        StatusLine = { fg = "#ebdbb2", bg = "#3c3836" },
+      }
     },
     -- Icons can be configured throughout the interface
     icons = {
